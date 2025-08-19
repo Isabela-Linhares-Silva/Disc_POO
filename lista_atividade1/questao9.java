9ed0cb68f6bda851b25d83ea7f87d0cc23496063
Isabela (Dev Local) <isabelalinharessilva@gmail.com>
2025-08-19 10:16:22 -0300

    ordem crescente valores

--- lista_atividade1/questao9.java
+++ lista_atividade1/questao9.java
@@ -6,8 +6,31 @@ public class questao9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Informe 3 valores: ");
-        
+        int v1 = sc.nextInt();
+        int v2 = sc.nextInt();
+        int v3 = sc.nextInt();
 
+        if (v1>v2 && v1>v3) {//v1 é maior
+            if (v2 > v3) { // v2 o do meio
+                System.out.println(" em ordem crescente: " + v3 + v2 + v1);
+            }
+            else //v3 o do meio
+                System.out.println(" em ordem crescente: " + v2 + v3 + v1);
+        }
+        else if (v2 > v3) {// v2 é maior
+            if (v1 > v3) {
+                System.out.println(" em ordem crescente: " + v3 + v1 + v2);
+            }
+            else 
+                System.out.println(" em ordem crescente: " + v1 + v3 + v2);   
+        }
+        else{// v3 é maior
+            if (v1>v2) {
+                System.out.println(" em ordem crescente: " + v2 + v1 + v3);
+            }
+            else
+                System.out.println(" em ordem crescente: " + v1 + v2 + v3);
+        }
         sc.close();
     }
 }
