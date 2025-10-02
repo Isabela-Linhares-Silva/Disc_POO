90f59a17bcf9e8cf9c886509c0aebb29c17b1b95
Isabela (Dev Local) <isabelalinharessilva@gmail.com>
2025-10-02 09:20:12 -0300

    comentario

--- Abstratos/GameProject.java
+++ Abstratos/GameProject.java
@@ -7,7 +7,7 @@ public class GameProject {
         Game game1 = new Fisico("GoW", 200.0f, "L01");
         Game game2 = new Digital("The Witcher", 75, "62626262");
 
-        ArrayList<Game> games = new ArrayList<>();
+        ArrayList<Game> games = new ArrayList<>();// essa lista vai receber apenas objetos do tipo game
         games.add(game1);
         games.add(game2);
 
